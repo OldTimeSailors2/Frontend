011f11bdd3a88be669f25c993ca4681f34d41a4f
OldTimeSailors2 <weboldtimesailors2@gmail.com>
2024-04-16 18:19:49 -0300

    .

--- src/components/LandingLeftSide.js
+++ src/components/LandingLeftSide.js
@@ -180,7 +180,7 @@ const LandingLeftSide = () => {
                 <Image
                   src={BlurbTitle}
                   width={130}
-                  height={20}
+                  height={44}
                   alt="Blurb title"
                   className="h-auto"
                   placeholder="blur"
