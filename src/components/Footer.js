db4ccd0dc3e04a8b8e6195d024e9bca99998e03d
Ignacio <nfcreaction@gmail.com>
2025-04-29 11:44:32 -0300

    Correcion footer y navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,10 @@
 import { usePathname } from "next/navigation";
 import { useLoader } from "@/context/LoaderContext";
 import { useState, useEffect } from "react";
-const Footer = () => {
-  const [isFooterBlack, setIsFooterBlack] = useState(false);
-  const pathname = usePathname();
-  const defaultSettings = { loader: 1, photos: true };
-  const settings = useLoader() || defaultSettings;
-
-  useEffect(() => {
-    if (pathname === "/tickets" || pathname === "/services") {
-      setIsFooterBlack(true);
-    } else setIsFooterBlack(false);
-  }, [pathname]);
-  return (
-    <footer
-      className={`fixed bottom-0 w-screen flex justify-center items-center  ${pathname === "/" ? "py-0.5 xl:p-0.5 2k:py-2" : "py-0.5 md1:py-2"}`}
 import { useNavbarColor } from "@/context/NavbarColorProvider";
 
 const Footer = () => {
-  const [isFooterBlack, setIsFooterBlack] = useState(true);
+  const [isFooterBlack, setIsFooterBlack] = useState(false);
   const pathname = usePathname();
   const defaultSettings = { loader: 1, photos: true };
   const settings = useLoader() || defaultSettings;
@@ -29,25 +15,29 @@ const Footer = () => {
   useEffect(() => {
     if (pathname === "/" || pathname === "/media" || pathname === "/reviews" || pathname === "/our-clients" || navbarColor === "light") {
       setIsFooterBlack(false);
-    } else setIsFooterBlack(true);
+    } else {
+      setIsFooterBlack(true);
+    }
   }, [pathname, navbarColor]);
+
   return (
     <footer
-      className={`bottom-0 w-screen flex justify-center items-center  ${pathname === "/" ? "py-0.5 xl:p-0.5 2k:py-2" : "py-0.5 md1:py-1.5"} ${
+      className={`fixed bottom-0 w-screen flex justify-center items-center  ${
+        pathname === "/" ? "py-0.5 xl:p-0.5 2k:py-2" : "py-0.5 md1:py-1.5"
+      } ${
         navbarColor === "dark" || pathname === "/tickets" ? "bg-beigePatternMobile bg-cover z-50" : ""
-      }${navbarColor === "light" ? "bg-darkBlue bg-cover z-50" : ""}
-      `}
-
+      } ${
+        navbarColor === "light" ? "bg-darkBlue bg-cover z-50" : ""
+      }`}
       style={{
         opacity: settings?.loader,
         pointerEvents: settings?.photos ? "auto" : "none",
       }}
     >
       <p
-
-        className={`${isFooterBlack ? "text-black" : "text-beige"} font-txt text-[9px] md1:text-lg md:text-xl xl:text-base 2k:text-3xl 4k:text-5xl uppercase `}
-        className={`${isFooterBlack ? "text-darkBlue" : "text-beige"} ${pathname === "/tickets/map-view" ? " backdrop-blur-[4px] z-50" : ""}
-          font-txt text-[9px] md1:text-lg md:text-xl xl:text-base 2k:text-3xl 4k:text-5xl uppercase `}
+        className={`${isFooterBlack ? "text-darkBlue" : "text-beige"} ${
+          pathname === "/tickets/map-view" ? "backdrop-blur-[4px] z-50" : ""
+        } font-txt text-[9px] md1:text-lg md:text-xl xl:text-base 2k:text-3xl 4k:text-5xl uppercase`}
       >
         OLD TIME SAILORS LTD.®
       </p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { useLoader } from "@/context/LoaderContext";
 import { usePathname } from "next/navigation";
@@ -11,6 +12,7 @@ import { FaWhatsapp, FaInstagram, FaFacebookF } from "react-icons/fa";
 import ViewSwitch from "./ViewSelectorSwitch";
 import { useNavbarColor } from "@/context/NavbarColorProvider";
 import useScrollTrigger from "@/hooks/useScrollTrigger";
+
 const Navbar = () => {
   const pathname = usePathname();
   const defaultSettings = { loader: 1, photos: true };
@@ -32,16 +34,19 @@ const Navbar = () => {
   ];
   const isDynamicRoute =
     pathname.startsWith("/tickets/") && !fixedRoutes.includes(pathname);
+
   const getBackgroundColor = (path) => {
     if (path === "/media") {
       return "rgba(35, 48, 64, 0.8)";
     } else if (path === "/reviews") {
       return "rgba(221, 50, 84, 0.8)";
-    } else return "transparent";
+    } else {
+      return "transparent";
+    }
   };
+
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
       if (window.scrollY > 1) {
         setNavStyle({
           boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
@@ -99,115 +104,6 @@ const Navbar = () => {
         setTitle("services");
         setTitleBg("redPattern");
         setTitleColor("beige");
-        break;
-      default:
-        setTitle("reviews");
-        setTitleBg("bluePattern");
-        setTitleColor("beige");
-        break;
-    }
-  }, [pathname]);
-  return (
-    <div
-      style={navStyle}
-      className={`fixed w-screen z-[100] flex justify-between pt-3 px-1 sm:px-4 ${pathname === "/tickets" ? "pointer-events-none" : ""}`}
-    >
-      <div className="flex gap-1.5 sm:gap-4 items-center">
-        <Link
-          href="/"
-          className="inline-block"
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-        >
-          <Image
-            src={!["/services", "/tickets"].includes(pathname) ? logo : logo2}
-            width={65}
-            height={65}
-            priority={true}
-            alt="OTS Logo"
-            className="xs:w-[80px] xs:h-[80px] sm:w-32 sm:h-32 fullHD:w-40 fullHD:h-40 2k:w-48 2k:h-48 4k:w-64 4k:h-64 pointer-events-auto"
-          />
-        </Link>
-        <h1
-          className={`octagon-navbar bg-${titleBg} bg-contain text-${titleColor} font-titles
-           text-2xl xs2:text-[26px] sm:text-[40px] fullHD:text-5xl 2k:text-7xl 4k:text-8xl
-            flex items-center justify-center fullHD:pb-0.5 fullHD:pl-2`}
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-        >
-          {title}
-        </h1>
-      </div>
-
-      <div className="flex gap-1.5 xs:gap-2 sm:gap-4 items-center">
-        <Link
-          className={`${pathname === "/tickets" || pathname === "/services" ? "bg-redPattern text-beige" : "bg-beigePattern text-lightRed"}
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-          href="mailto:captainnicholasmoffat@oldtimesailors.com"
-          target="_blank"
-        >
-          <TfiEmail
-            size={22}
-            className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px]  4k:w-[60px] 4k:h-[60px]"
-          />
-        </Link>
-
-        <Link
-          className={`${pathname === "/tickets" || pathname === "/services" ? "bg-bluePattern text-beige" : "bg-beigePattern text-darkBlue"}
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-          href="https://wa.me/447539045312"
-          target="_blank"
-        >
-          <FaWhatsapp
-            size={22}
-            className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px] 4k:w-[60px] 4k:h-[60px]"
-          />
-        </Link>
-
-        <Link
-          className={`${pathname === "/tickets" || pathname === "/services" ? "bg-redPattern text-beige" : "bg-beigePattern text-lightRed"}
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-          href="https://www.instagram.com/oldtimesailors"
-          target="_blank"
-        >
-          <FaInstagram
-            size={22}
-            className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px] 4k:w-[60px] 4k:h-[60px]"
-          />
-        </Link>
-
-        <Link
-          className={`${pathname === "/tickets" || pathname === "/services" ? "bg-bluePattern text-beige" : "bg-beigePattern text-darkBlue"}
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
-          style={{
-            opacity: settings?.loader,
-            pointerEvents: settings?.photos ? "auto" : "none",
-          }}
-          href="https://www.facebook.com/oldtimesailors/"
-          target="_blank"
-        >
-          <FaFacebookF
-            size={22}
-            className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px] 4k:w-[60px] 4k:h-[60px]"
-          />
-        </Link>
-      </div>
         setViewSelector(false);
         break;
       default:
@@ -220,9 +116,6 @@ const Navbar = () => {
   }, [pathname]);
 
   const isScrolled = useScrollTrigger({ threshold: 0 });
-  useEffect(() => {
-    console.log("Scroll state:", isScrolled);
-  }, [isScrolled]);
 
   const getNavbarBackground = () => {
     if (!isDynamicRoute || !isScrolled) return "";
@@ -237,9 +130,9 @@ const Navbar = () => {
   };
 
   const getNavbarStyle = () => {
-    if (isDynamicRoute && isScrolled && navbarColor === 'light') {
+    if (isDynamicRoute && isScrolled && navbarColor === "light") {
       return {
-        backgroundColor: '#1f344a'
+        backgroundColor: "#1f344a",
       };
     }
     return {};
@@ -247,8 +140,8 @@ const Navbar = () => {
 
   return (
     <div
-      style={getNavbarStyle()}
-      className={`fixed w-screen z-[100] flex flex-col justify-between pt-3 px-1 sm:px-4 3xl:px-9  ${
+      style={{ ...navStyle, ...getNavbarStyle() }}
+      className={`fixed w-screen z-[100] flex flex-col justify-between pt-3 px-1 sm:px-4 3xl:px-9 ${
         pathname === "/tickets/calendar-view" || pathname === "/tickets"
           ? "bg-beigePatternMobile bg-cover"
           : ""
@@ -299,14 +192,13 @@ const Navbar = () => {
         <div className="flex gap-1.5 xs:gap-2 sm:gap-4 items-center">
           <Link
             className={`${
-              pathname != "/media" &&
-              pathname != "/reviews" &&
-              pathname != "/our-clients" &&
+              pathname !== "/media" &&
+              pathname !== "/reviews" &&
+              pathname !== "/our-clients" &&
               navbarColor === "dark"
                 ? "bg-redPattern text-beige"
                 : "bg-beigePattern text-lightRed"
-            }
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
+            } bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
             style={{
               opacity: settings?.loader,
               pointerEvents: settings?.photos ? "auto" : "none",
@@ -316,20 +208,19 @@ const Navbar = () => {
           >
             <TfiEmail
               size={22}
-              className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px]  4k:w-[60px] 4k:h-[60px]"
+              className="xs:w-[24px] xs:h-[24px] iphone-3:w-[26px] iphone-3:h-[26px] sm:w-[30px] sm:h-[30px] 2k:w-[50px] 2k:h-[50px] 4k:w-[60px] 4k:h-[60px]"
             />
           </Link>
 
           <Link
             className={`${
               navbarColor === "dark" &&
-              pathname != "/media" &&
-              pathname != "/reviews" &&
-              pathname != "/our-clients"
+              pathname !== "/media" &&
+              pathname !== "/reviews" &&
+              pathname !== "/our-clients"
                 ? "bg-bluePattern text-beige"
                 : "bg-beigePattern text-darkBlue"
-            }
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
+            } bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
             style={{
               opacity: settings?.loader,
               pointerEvents: settings?.photos ? "auto" : "none",
@@ -346,13 +237,12 @@ const Navbar = () => {
           <Link
             className={`${
               navbarColor === "dark" &&
-              pathname != "/media" &&
-              pathname != "/reviews" &&
-              pathname != "/our-clients"
+              pathname !== "/media" &&
+              pathname !== "/reviews" &&
+              pathname !== "/our-clients"
                 ? "bg-redPattern text-beige"
                 : "bg-beigePattern text-lightRed"
-            }
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
+            } bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
             style={{
               opacity: settings?.loader,
               pointerEvents: settings?.photos ? "auto" : "none",
@@ -369,13 +259,12 @@ const Navbar = () => {
           <Link
             className={`${
               navbarColor === "dark" &&
-              pathname != "/media" &&
-              pathname != "/reviews" &&
-              pathname != "/our-clients"
+              pathname !== "/media" &&
+              pathname !== "/reviews" &&
+              pathname !== "/our-clients"
                 ? "bg-bluePattern text-beige"
                 : "bg-beigePattern text-darkBlue"
-            }
-                                bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
+            } bg-contain rounded-full p-1 sm:p-2 2k:p-3 4k:p-3.5 pointer-events-auto`}
             style={{
               opacity: settings?.loader,
               pointerEvents: settings?.photos ? "auto" : "none",
@@ -390,7 +279,7 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
-      {viewSelector === true ? <ViewSwitch /> : false}
+      {viewSelector && <ViewSwitch />}
     </div>
   );
 };
