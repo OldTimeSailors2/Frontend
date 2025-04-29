bfc06232fa91342c2ede1276dd2d130e171936df
Ignacio <nfcreaction@gmail.com>
2025-04-29 12:24:39 -0300

    Navbar correcciones

--- src/context/NavbarColorProvider.js
+++ src/context/NavbarColorProvider.js
@@ -1,14 +1,27 @@
 import { createContext, useContext, useState } from "react";
 
 // Crea el contexto
-const NavbarColorContext = createContext();
+const NavbarColorContext = createContext({
+  navbarColor: "light", // Valor predeterminado
+  setNavbarColor: () => {}, // Función vacía predeterminada
+});
 
 // Hook para usar el contexto
-export const useNavbarColor = () => useContext(NavbarColorContext);
+export const useNavbarColor = () => {
+  const context = useContext(NavbarColorContext);
+  if (context === undefined) {
+    throw new Error('useNavbarColor must be used within a NavbarColorProvider');
+  }
+  return context;
+};
 
 // Proveedor del contexto
 export const NavbarColorProvider = ({ children }) => {
-  const [navbarColor, setNavbarColor] = useState("defaultColor");
+  const [navbarColor, setNavbarColor] = useState("light"); // Valor inicial
 
-  return <NavbarColorContext.Provider value={{ navbarColor, setNavbarColor }}>{children}</NavbarColorContext.Provider>;
+  return (
+    <NavbarColorContext.Provider value={{ navbarColor, setNavbarColor }}>
+      {children}
+    </NavbarColorContext.Provider>
+  );
 };
